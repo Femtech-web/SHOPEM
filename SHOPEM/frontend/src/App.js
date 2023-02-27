f924fe11c78d0acb7f563ac52ab36469c1fee3ae
Adeyemi-Femi <bossfemzy@gmail.com>
2023-02-27 11:25:27 +0100

    added an update

--- SHOPEM/frontend/src/App.js
+++ SHOPEM/frontend/src/App.js
@@ -19,7 +19,7 @@ function App() {
 
   return (
     <>
-    <Router>
+    <Router basename='/'>
       <Routes>
         <Route exact path="/" element={<Home />}/>
         <Route exact path="/Register" element={<Register />}/>
